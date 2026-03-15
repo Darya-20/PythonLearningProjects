6f6005acc361e4d7f95dc919eafdac98f1441fa6
Darya-20 <darya.suhowa2016@yandex.ru>
2026-03-15 19:19:36 +0400

    Финальный проект - API для ФСТР. Создание приложения fstr_api

---
+++ FINAL_PROJECT/project/fstr_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FstrApiConfig(AppConfig):
+    name = 'fstr_api'
--- FINAL_PROJECT/project/project/settings.py
+++ FINAL_PROJECT/project/project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'fstr_api',
+    'rest_framework',
+    'drf-spectacular',
 ]
 
 MIDDLEWARE = [
